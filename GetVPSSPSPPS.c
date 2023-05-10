8c35c86c60e7f9efc643ba65bae8776a213cd7d1
Tangtao Qin <qintangtao@qq.com>
2023-05-10 17:30:28 +0800

    use media util

--- GetVPSSPSPPS.c
+++ GetVPSSPSPPS.c
@@ -1,75 +1,19 @@
 #include "GetVPSSPSPPS.h"
 #include <string.h>
+#include "media_util.h"
 
-static uint8 * avc_find_startcode_internal(uint8 *p, uint8 *end)
+
+int GetH265VPSandSPSandPPS(uint8_t *p_data, int len, uint8_t *_vps, int *_vpslen, uint8_t *_sps, int *_spslen, uint8_t *_pps, int *_ppslen)
 {
-	uint8 *a = p + 4 - ((intptr_t)p & 3);
-
-	for (end -= 3; p < a && p < end; p++)
-	{
-		if (p[0] == 0 && p[1] == 0 && p[2] == 1)
-		{
-			return p;
-		}
-	}
-
-	for (end -= 3; p < end; p += 4)
-	{
-		uint32 x = *(const uint32*)p;
-
-		if ((x - 0x01010101) & (~x) & 0x80808080)
-		{ // generic
-			if (p[1] == 0)
-			{
-				if (p[0] == 0 && p[2] == 1)
-					return p;
-				if (p[2] == 0 && p[3] == 1)
-					return p + 1;
-			}
-
-			if (p[3] == 0)
-			{
-				if (p[2] == 0 && p[4] == 1)
-					return p + 2;
-				if (p[4] == 0 && p[5] == 1)
-					return p + 3;
-			}
-		}
-	}
-
-	for (end += 3; p < end; p++)
-	{
-		if (p[0] == 0 && p[1] == 0 && p[2] == 1)
-		{
-			return p;
-		}
-	}
-
-	return end + 3;
-}
-
-static uint8 * avc_find_startcode(uint8 *p, uint8 *end)
-{
-	uint8 *out = avc_find_startcode_internal(p, end);
-	if (p < out && out < end && !out[-1])
-	{
-		out--;
-	}
-
-	return out;
-}
-
-int GetH265VPSandSPSandPPS(uint8 *p_data, int len, uint8 *_vps, int *_vpslen, uint8 *_sps, int *_spslen, uint8 *_pps, int *_ppslen)
-{
-	uint8 nalu_t; int nalu_len;
-	uint8 *r, *end = p_data + len;
+	uint8_t nalu_t; int nalu_len;
+	uint8_t *r, *end = p_data + len;
 	*_vpslen = 0, *_spslen = 0; *_ppslen = 0;
 
 	r = avc_find_startcode(p_data, end);
 
 	while (r < end)
 	{
-		uint8 *r1;
+		uint8_t *r1;
 
 		while (!*(r++));
 		r1 = avc_find_startcode(r, end);
@@ -98,17 +42,17 @@ int GetH265VPSandSPSandPPS(uint8 *p_data, int len, uint8 *_vps, int *_vpslen, ui
 	return (*_vpslen > 0 && *_spslen > 0 && *_ppslen > 0) ? 0 : -1;
 }
 
-int GetH264SPSandPPS(uint8 *p_data, int len, uint8 *_sps, int *_spslen, uint8 *_pps, int *_ppslen)
+int GetH264SPSandPPS(uint8_t *p_data, int len, uint8_t *_sps, int *_spslen, uint8_t *_pps, int *_ppslen)
 {
-	uint8 nalu_t; int nalu_len;
-	uint8 *r, *end = p_data + len;
+	uint8_t nalu_t; int nalu_len;
+	uint8_t *r, *end = p_data + len;
 	*_spslen = 0; *_ppslen = 0;
 
 	r = avc_find_startcode(p_data, end);
 
 	while (r < end)
 	{
-		uint8 *r1;
+		uint8_t *r1;
 
 		while (!*(r++));
 		r1 = avc_find_startcode(r, end);
--- GetVPSSPSPPS.h
+++ GetVPSSPSPPS.h
@@ -1,19 +1,16 @@
 #ifndef GETVPSSPSPPS_H
 #define GETVPSSPSPPS_H
+#include <stdint.h>
 
-
-//输入的pbuf必须包含start code(00 00 00 01)
-
-typedef unsigned char uint8;
-typedef unsigned int uint32;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+//输入的pbuf必须包含start code(00 00 00 01)
 
-int GetH265VPSandSPSandPPS(uint8 *pbuf, int bufsize, uint8 *_vps, int *_vpslen, uint8 *_sps, int *_spslen, uint8 *_pps, int *_ppslen);
-int GetH264SPSandPPS(uint8 *pbuf, int bufsize, uint8 *_sps, int *_spslen, uint8 *_pps, int *_ppslen);
+int GetH265VPSandSPSandPPS(uint8_t *pbuf, int bufsize, uint8_t *_vps, int *_vpslen, uint8_t *_sps, int *_spslen, uint8_t *_pps, int *_ppslen);
+int GetH264SPSandPPS(uint8_t *pbuf, int bufsize, uint8_t *_sps, int *_spslen, uint8_t *_pps, int *_ppslen);
 
 #ifdef __cplusplus
 }
